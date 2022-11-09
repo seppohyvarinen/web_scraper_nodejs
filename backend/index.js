b346d42a93113617cc541e9aa85d59e7310cbeb7
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2022-11-09 15:02:02 +0200

    Add: first test scrape complete, print song name from youtube to console

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,15 @@
 const puppeteer = require("puppeteer");
 
-const main = (greetings) => {
+const main = async (greetings) => {
+  const browser = await puppeteer.launch({});
+  const page = await browser.newPage();
+  await page.goto("https://www.youtube.com/results?search_query=gangnam+style");
+  let element = await page.waitForSelector(
+    "#video-title > yt-formatted-string"
+  );
+  var text = await page.evaluate((element) => element.textContent, element);
+  console.log(text);
+
   console.log(greetings);
 };
 
