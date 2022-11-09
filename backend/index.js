687bb98dc97aad6e22d21960d207969b63fb12f4
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2022-11-09 19:00:49 +0200

    Add: routing for backend, simple express route

--- backend/index.js
+++ backend/index.js
@@ -2,23 +2,36 @@ const puppeteer = require("puppeteer");
 const express = require("express");
 const app = express();
 
-var cors = require("cors");
+let results = express.Router();
+
+let cors = require("cors");
 app.use(cors());
 app.use(express.static("../frontend/build"));
 const port = 8080;
+app.use("/results", results);
 
-const main = async (greetings) => {
+results.get("/", async (req, res) => {
+  try {
+    let all = await main();
+    res.send(all);
+  } catch (error) {
+    res.statusCode = 404;
+    res.send(error);
+  }
+});
+
+const main = async () => {
   const browser = await puppeteer.launch({});
   const page = await browser.newPage();
   await page.goto("https://www.youtube.com/results?search_query=gangnam+style");
   let element = await page.waitForSelector(
     "#video-title > yt-formatted-string"
   );
-  var text = await page.evaluate((element) => element.textContent, element);
+  let text = await page.evaluate((element) => element.textContent, element);
   console.log(text);
   browser.close();
 
-  console.log(greetings);
+  return text;
 };
 
 const server = app.listen(port, () => {
