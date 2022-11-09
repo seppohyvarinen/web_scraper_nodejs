d369a535e791aef20dcb3b33d7235091cdbf2365
seppohyvarinen <seppo.hyvarinen@tuni.fi>
2022-11-09 18:55:56 +0200

    Add: initialize scraperframe with button and empty function, ready for scraping

--- frontend/src/components/ScraperFrame.js
+++ frontend/src/components/ScraperFrame.js
@@ -1,5 +1,18 @@
+import { useEffect, useState } from "react";
+
+const axios = require("axios").default;
+
 const ScraperFrame = () => {
-  return <div>Scrape results here</div>;
+  const [scraped, setScraped] = useState("scrape here");
+
+  const scrape = async () => {};
+
+  return (
+    <div>
+      <button onClick={() => scrape()}>scrape</button>
+      <div>{scraped}</div>;
+    </div>
+  );
 };
 
 export default ScraperFrame;
